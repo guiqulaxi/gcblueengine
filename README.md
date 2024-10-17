1418ea41ef09ffc082daa126fb3b5476520bb0a5
kang sun <935519890@qq.com>
2024-10-17 23:16:23 +0800

    修改readme启动说明

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 4. 用http,json对外开放接口（去除会加速很多）;  
 5. 修改了一些bug。  
 ## 运行
-gcblue --scenario scenarios/SinglePlayer/Modern/Odyssey_Dawn.py --times 100 --runmode debug --ip  0.0.0.0 --port 8080
+--scenario scenarios/Tutorial/SateliteOperations.py --times 100 --runmode normal --ip  0.0.0.0 --port 8081
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set PYTHONHOME=C:\Users\93551\.conda\envs\test_pybind11
 set PYTHONHOME=C:\Users\93551\.conda\envs\test_pybind11
 set PATH=%PATH%;C:\Users\93551\.conda\envs\test_pybind11;
-gcblue.exe --scenario scenarios/SinglePlayer/Modern/Odyssey_Dawn.py --times 100 --runmode debug --ip  0.0.0.0 --port 8081
+gcblue.exe --scenario scenarios/Tutorial/SateliteOperations.py --times 100 --runmode normal --ip  0.0.0.0 --port 8081
 
 
